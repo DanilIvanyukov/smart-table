cbda0cfad95d3fb73451045cc7e6113371d0ed81
Danil Ivanyukov <danil.ivanyukov@yandex.ru>
2025-11-10 11:55:59 +0400

    test

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import {data as sourceData} from "./data/dataset_1.js";
 
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
-
+import {initPagination} from "./components/pagination.js"
 import {initTable} from "./components/table.js";
+import { initSorting } from './components/sorting.js';
+import {initFiltering } from './components/filtering.js'
 // @todo: подключение
 
 
@@ -19,9 +21,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -33,6 +39,8 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applyPagination(result, state, action);
+    result = applySorting(result, state, action);
 
 
     sampleTable.render(result)
@@ -41,12 +49,31 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['header', 'filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const applyPagination = initPagination(
+    sampleTable.pagination.elements, 
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+); 
 
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {    
+    searchBySeller: indexes.sellers                                   
+}); 
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
